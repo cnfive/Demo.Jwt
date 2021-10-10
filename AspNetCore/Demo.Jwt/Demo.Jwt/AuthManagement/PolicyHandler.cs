c08279d456bb110696c4268c7ca662255c5596a5
yang <37206103+cnfive@users.noreply.github.com>
2021-10-10 15:13:59 +0800

    Add files via upload

--- AspNetCore/Demo.Jwt/Demo.Jwt/AuthManagement/PolicyHandler.cs
+++ AspNetCore/Demo.Jwt/Demo.Jwt/AuthManagement/PolicyHandler.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
-namespace Demo.Jwt.AuthManagement
+namespace WebApplication5.AuthManagement
 {
     public class PolicyHandler : AuthorizationHandler<PolicyRequirement>
     {
@@ -12,25 +12,42 @@ namespace Demo.Jwt.AuthManagement
             //赋值用户权限
             var userPermissions = requirement.UserPermissions;
             //从AuthorizationHandlerContext转成HttpContext，以便取出表求信息
-            var httpContext = (context.Resource as Microsoft.AspNetCore.Mvc.Filters.AuthorizationFilterContext).HttpContext;
+          //  var httpContext = (context.Resource as Microsoft.AspNetCore.Mvc.Filters.AuthorizationFilterContext).HttpContext;
             //请求Url
-            var questUrl = httpContext.Request.Path.Value.ToUpperInvariant();
+     //       var questUrl = httpContext.Request.Path.Value.ToUpperInvariant();
             //是否经过验证
-            var isAuthenticated = httpContext.User.Identity.IsAuthenticated;
+       //     var isAuthenticated = httpContext.User.Identity.IsAuthenticated;
+
+            var questUrl = (context.Resource as Microsoft.AspNetCore.Routing.RouteEndpoint).RoutePattern.RawText.ToString();
+
+            //是否经过验证
+
+            var isAuthenticated = context.User.Identity.IsAuthenticated;
+
+          //  var isAuthenticated = context.User.Identity.IsAuthenticated;
+
+
             if (isAuthenticated)
             {
                 if (userPermissions.GroupBy(g => g.Url).Any(w => w.Key.ToUpperInvariant() == questUrl))
                 {
                     //用户名
-                    var userName = httpContext.User.Claims.SingleOrDefault(s => s.Type == ClaimTypes.NameIdentifier).Value;
-                    if (userPermissions.Any(w => w.UserName == userName && w.Url.ToUpperInvariant() == questUrl))
+                //    var userName = httpContext.User.Claims.SingleOrDefault(s => s.Type == ClaimTypes.NameIdentifier).Value;
+
+                    var userName = context.User.Claims.SingleOrDefault(s => s.Type == ClaimTypes.NameIdentifier).Value;
+                    //     if (userPermissions.Any(w => w.UserName == userName && w.Url.ToUpperInvariant() == questUrl))
+
+                    //     if (userPermissions.Any(w => w.RoleName == userName && w.Url == questUrl.ToString()))
+                    if (userPermissions.Any(w => w.UserName == userName && w.Url == questUrl.ToString()))
                     {
                         context.Succeed(requirement);
                     }
                     else
                     {
                         //无权限跳转到拒绝页面
-                        httpContext.Response.Redirect(requirement.DeniedAction);
+                        //  httpContext.Response.Redirect(requirement.DeniedAction);
+                        context.Fail();
+                      //  context.respon
                     }
                 }
                 else
@@ -42,3 +59,5 @@ namespace Demo.Jwt.AuthManagement
         }
     }
 }
+
+
